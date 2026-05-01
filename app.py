8771342e838b9047bb824e67b6610afb2eecccf6
Bhargav Sharma <bhargavsharma71@gmail.com>
2026-05-01 12:58:06 +0530

    Fixed calculation logic

--- app.py
+++ app.py
@@ -8,20 +8,27 @@ def home():
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    num1 = float(request.form['num1'])
-    num2 = float(request.form['num2'])
-    op = request.form['operation']
+    try:
+        num1 = float(request.form.get('num1', 0))
+        num2 = float(request.form.get('num2', 0))
+        op = request.form.get('operation')
 
-    if op == 'add':
-        result = num1 + num2
-    elif op == 'sub':
-        result = num1 - num2
-    elif op == 'mul':
-        result = num1 * num2
-    elif op == 'div':
-        result = num1 / num2
-    else:
-        result = "Error"
+        if op == 'add':
+            result = num1 + num2
+        elif op == 'sub':
+            result = num1 - num2
+        elif op == 'mul':
+            result = num1 * num2
+        elif op == 'div':
+            if num2 == 0:
+                result = "Cannot divide by zero"
+            else:
+                result = num1 / num2
+        else:
+            result = "Invalid operation"
+
+    except Exception as e:
+        result = "Invalid input"
 
     return render_template("index.html", result=result)
 
